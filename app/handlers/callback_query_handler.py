e23f6fa77d2f6adf29b00864e8d118d378b7caaa
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-01-31 16:16:59 +0300

    feat: finish pagination in catalog menu

--- app/handlers/callback_query_handler.py
+++ app/handlers/callback_query_handler.py
@@ -19,17 +19,48 @@ CATALOG_MENU = InlineKeyboardMarkup().add(BTN_BACK, BTN_CURRENT_PAGE, BTN_NEXT).
 
 async def show_catalog(query: CallbackQuery):
     await query.bot.answer_callback_query(query.id)
-    # a = query.data.split('_')
-    # print(a)
-    # b = json_loads(a[0])
-    # print(b)
-    count_products = get_count_all_products()
-    products = get_all_products()
+    request = query.data.split('_')
+    print(request)
+    if request[0] == 'menu':
+       await query.bot.delete_message(query.message.chat.id, query.message.message_id)
+    elif "pagin" in request[0]:
+        json_string = json_loads(request[0])
+        page = int(json_string['PageNum'])
+        count = json_string['CountPage']
+        slicer = int(5 * page)
+        print('строка json')
+        print(json_string)
 
-    if count_products > 5:
-        pass
-    
-    await query.bot.send_message(query.from_user.id, text='Мы готовим...', reply_markup=CATALOG_MENU)
+
+
+        
+        products = get_all_products()
+
+        markup = InlineKeyboardMarkup()
+        for product in products[slicer - 5:slicer]:
+                markup.add(InlineKeyboardButton(str(product), callback_data=' '))
+        if page == 1:
+            markup.add(
+                InlineKeyboardButton('<--', callback_data=" "),
+                InlineKeyboardButton(f'{page}/{count}', callback_data=" "),
+                InlineKeyboardButton('-->', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":" + str(page + 1)+ ",\"CountPage\":" + str(count)+"}"),
+            )
+        elif page == count:
+            markup.add(
+                InlineKeyboardButton('<--', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":" + str(page - 1)+ ",\"CountPage\":" + str(count)+"}"),
+                InlineKeyboardButton(f'{page}/{count}', callback_data=" "),
+                InlineKeyboardButton('-->', callback_data=" "),
+            )
+        else:
+            markup.add(
+                InlineKeyboardButton('<--', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":" + str(page - 1)+ ",\"CountPage\":" + str(count)+"}"),
+                InlineKeyboardButton(f'{page}/{count}', callback_data=" "),
+                InlineKeyboardButton('-->', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":" + str(page + 1)+ ",\"CountPage\":" + str(count)+"}"),
+            )
+
+        markup.add(BTN_MENU)
+        
+        await query.bot.send_message(query.from_user.id, text='Мы готовим...', reply_markup=markup)
 
     
 
--- app/handlers/command_handler.py
+++ app/handlers/command_handler.py
@@ -1,13 +1,13 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, InputFile, CallbackQuery
 from app.config.config import START_LOGO
-from app.database.methods.get import get_all_products
+from app.database.methods.get import get_all_products, get_count_all_products
 
 
-# btns
+# btns()
 BTN_WEB_SITE = InlineKeyboardButton('На сайт', callback_data='web_site')
 BTN_DESCRIPTION = InlineKeyboardButton('Процесс готовки', callback_data='description')
-BTN_CATALOG = InlineKeyboardButton('Каталог', callback_data="{\"page\":\"catalog\"}")
+BTN_CATALOG = InlineKeyboardButton('Каталог', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":\"s\",\"CountPage\":\"s\"}")
 # keyboard
 START_MENU = InlineKeyboardMarkup().add(BTN_WEB_SITE).add(BTN_DESCRIPTION).add(BTN_CATALOG)
 START_MENU1 = InlineKeyboardMarkup().add(BTN_CATALOG)
@@ -16,8 +16,13 @@ START_MENU1 = InlineKeyboardMarkup().add(BTN_CATALOG)
 # handlers
 # start msg
 async def send_welcome_msg(msg: Message):
+    count_products = get_count_all_products()
+    count = round(count_products / 5) + 1
+    markup = InlineKeyboardMarkup().add(InlineKeyboardButton('На сайт', callback_data='web_site'))
+    markup.add(InlineKeyboardButton('Процесс готовки', callback_data='description'))
+    markup.add(InlineKeyboardButton('Каталог', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":\"1\",\"CountPage\":"+ str(count) +"}"))
     with open(START_LOGO, 'rb') as img:
-        await msg.bot.send_photo(msg.chat.id, photo=InputFile(img), caption='Привет!', reply_markup=START_MENU)
+        await msg.bot.send_photo(msg.chat.id, photo=InputFile(img), caption='Привет!', reply_markup=markup)
 
 
 def register_command_handlers(dp: Dispatcher):
