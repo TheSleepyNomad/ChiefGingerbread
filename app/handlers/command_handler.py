eb85de43809c43aeddf49a1090bae6a91c5c5bd1
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-02-05 14:49:42 +0300

    refactor(command_handler.py): delete useless code

--- app/handlers/command_handler.py
+++ app/handlers/command_handler.py
@@ -6,30 +6,16 @@ from app.database.methods.other import check_user_baket_exist
 from math import ceil
 
 
-# btns()
-BTN_WEB_SITE = InlineKeyboardButton('На сайт', callback_data='web_site')
-BTN_DESCRIPTION = InlineKeyboardButton('Процесс готовки', callback_data='description')
-BTN_CATALOG = InlineKeyboardButton('Каталог', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":\"s\",\"CountPage\":\"s\"}")
-# keyboard
-START_MENU = InlineKeyboardMarkup().add(BTN_WEB_SITE).add(BTN_DESCRIPTION).add(BTN_CATALOG)
-START_MENU1 = InlineKeyboardMarkup().add(BTN_CATALOG)
-
-
-# handlers
 # start msg
 async def send_welcome_msg(msg: Message):
-    print(msg.chat.id)
-    print(msg)
     count_products = get_count_all_products()
     count = ceil(count_products / 5)
     markup = InlineKeyboardMarkup().add(InlineKeyboardButton('На сайт', callback_data='web_site'))
     markup.add(InlineKeyboardButton('Процесс готовки', callback_data='description'))
     markup.add(InlineKeyboardButton('Каталог', callback_data="{\"page\":\"catalog\",\"act\":\"pagin\",\"PageNum\":\"1\",\"CountPage\":"+ str(count) +"}"))
-    print(check_user_baket_exist(msg.chat.id))
     if check_user_baket_exist(msg.chat.id):
         # think about get count method from db
         user_products_count = ceil(len(get_cart_by_user(msg.chat.id)) / 5)
-        print(user_products_count)
         markup.add(InlineKeyboardButton('Корзина', callback_data="{\"page\":\"cart\",\"act\":\"pagin\",\"PageNum\":\"1\",\"CountPage\":" + str(user_products_count) + "}"))
         
     with open(START_LOGO, 'rb') as img:
