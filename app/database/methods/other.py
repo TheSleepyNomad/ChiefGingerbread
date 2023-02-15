742e895685ad23dc9c72349bd4e4fe5765855270
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-02-15 14:51:35 +0300

    style: add comments and description for show_product_card and add_in_order

--- app/database/methods/other.py
+++ app/database/methods/other.py
@@ -5,11 +5,14 @@ from app.database.models import Products, Order
 
 def check_user_baket_exist(user_telegram_id: int) -> bool:
         session = Database().session
-        try:
-            session.query(Order.id).filter(Order.user_telegram_id == user_telegram_id).all()
-            session.close()
-            return True
-        except NoResultFound:
-            session.close()
-            return False
+        cart = session.query(Order.id).filter_by(user_telegram_id=user_telegram_id).exists()
+        session.close()
+        return True if cart else False
+        # try:
+        #     cart = session.query(Order.id).filter_by(user_telegram_id=user_telegram_id).exists()
+        #     session.close()
+        #     return True
+        # except NoResultFound:
+        #     session.close()
+        #     return False
         
